357a8a2cd2dd1981efb1a0969fca9ae195fe2c1f
tagplus5 <tagplus5@gmail.com>
2019-07-20 20:24:44 +0300

    update docker

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+BASEDIR=`dirname "$0"`
+FULLPATH=`cd "$BASEDIR"; pwd`
+cd $FULLPATH
+
 docker run --rm \
 -v /etc/localtime:/etc/localtime:ro \
 -v /home/backup/zenmoney:/app/export \
-tagplus5/zenmoney-backup sh -c "node app.js --login=LOGIN --pass=PASSWORD"
+-v $FULLPATH/zenmoney.json:/app/zenmoney.json \
+tagplus5/zenmoney-backup
